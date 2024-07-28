22eb9546b30692589d5ef66c7dff7e9a877f6d49
Volodymyr Stoiko <me@volodymyrstoiko.com>
2024-07-28 15:21:50 +0300

    Add health data structs

---
+++ health.go
@@ -0,0 +1,22 @@
+package api
+
+import v1 "k8s.io/api/core/v1"
+
+type HealthWorker struct {
+	// Data set on start
+	Hostname  string `json:"hostname"`
+	ClusterID string `json:"clusterID"`
+
+	Sniffer *HealthWorkerComponent `json:"sniffer"`
+	Tracer  *HealthWorkerComponent `json:"tracer"`
+}
+
+type HealthWorkerComponent struct {
+	CPUUsage          float64                 `json:"cpuUsage"`
+	MemoryAlloc       uint64                  `json:"memoryAlloc"`
+	MemoryUsage       float64                 `json:"memoryUsage"`
+	CPUNum            float64                 `json:"cpuNum"`
+	LastRestartReason string                  `json:"lastRestartReason"`
+	Resources         v1.ResourceRequirements `json:"resources"`
+	Restarts          int                     `json:"restarts"`
+}
