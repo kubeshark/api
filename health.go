55ef2ec562e09f10676aa6324b218fadffbd0ad0
Volodymyr Stoiko <me@volodymyrstoiko.com>
2024-07-30 09:08:46 +0300

    Revert "Add status field to health"
    This reverts commit 27b34de9b6febb3a5429523b523b1cdaafb18701.

--- health.go
+++ health.go
@@ -6,7 +6,6 @@ type HealthWorker struct {
 	// Data set on start
 	Hostname  string `json:"hostname"`
 	ClusterID string `json:"clusterID"`
-	Status    string `json:"status"`
 
 	Sniffer *HealthWorkerComponent `json:"sniffer"`
 	Tracer  *HealthWorkerComponent `json:"tracer"`
