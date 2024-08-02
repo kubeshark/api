1d64e58c680748d650b030dfdb44096648e86389
Volodymyr Stoiko <me@volodymyrstoiko.com>
2024-08-03 02:48:29 +0300

    Add health structs (#9)
    * Add health data structs
    
    * Remove cpunum
    
    * Add status field to health
    
    * Revert "Add status field to health"
    
    This reverts commit 27b34de9b6febb3a5429523b523b1cdaafb18701.
    
    * Add timestamp
    
    * Add hub health
    
    * Add map of strings
    
    * Use proper worker struct

---
+++ health.go
@@ -0,0 +1,40 @@
+package api
+
+import v1 "k8s.io/api/core/v1"
+
+type HealthWorker struct {
+	// Data set on start
+	Hostname  string `json:"hostname"`
+	ClusterID string `json:"clusterID"`
+
+	Sniffer *HealthWorkerComponent `json:"sniffer"`
+	Tracer  *HealthWorkerComponent `json:"tracer"`
+}
+
+type HealthWorkerComponent struct {
+	Timestamp         string                  `json:"timestamp"`
+	CPUUsage          float64                 `json:"cpuUsage"`
+	MemoryAlloc       uint64                  `json:"memoryAlloc"`
+	MemoryUsage       float64                 `json:"memoryUsage"`
+	LastRestartReason string                  `json:"lastRestartReason"`
+	Resources         v1.ResourceRequirements `json:"resources"`
+	Restarts          int                     `json:"restarts"`
+}
+
+type HealthHub struct {
+	Workers           []HealthHubWorker       `json:"workers"`
+	Hostname          string                  `json:"hostname"`
+	ClusterID         string                  `json:"clusterID"`
+	Timestamp         string                  `json:"timestamp"`
+	CPUUsage          float64                 `json:"cpuUsage"`
+	MemoryAlloc       uint64                  `json:"memoryAlloc"`
+	MemoryUsage       float64                 `json:"memoryUsage"`
+	LastRestartReason string                  `json:"lastRestartReason"`
+	Resources         v1.ResourceRequirements `json:"resources"`
+	Restarts          int                     `json:"restarts"`
+}
+
+type HealthHubWorker struct {
+	Addr    string `json:"addr"`
+	PodName string `json:"podName"`
+}
