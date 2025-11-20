bfcc00f3fb2678969f2c8d13def089bd9dc6a2d4
bogdanvbalan <32824086+bogdanvbalan@users.noreply.github.com>
2025-11-20 14:56:38 +0200

    Dissector stability (#28)
    * Add Flow ID
    
    * Update sig
    
    * Revert change
    
    * Update def
    
    * Sync with master
    
    * Update sig
    
    * Update def
    
    * Remove merge leftover
    
    ---------
    
    Co-authored-by: Ilya Gavrilov <gilyav@gmail.com>
    Co-authored-by: bogdan.balan1 <bogdanvalentin.balan@1nce.com>

--- api.go
+++ api.go
@@ -1125,6 +1125,9 @@ type TcpReader interface {
 
 type TcpStream interface {
 	SetProtocol(protocol *Protocol)
+	SetProtocolOnce(protocol *Protocol)
+	HasProtocol() bool
+	GetProtocol() (*Protocol, bool)
 	GetPcapId() string
 	GetIndex() int64
 	GetReqResMatchers() []RequestResponseMatcher
